bb75c00c7a9fedb688d648e39c2074ffcac470cb
hhhtak <hhhtak@users.noreply.github.com>
2024-07-13 15:20:21 +0900

    react fook form, zod追加

---
+++ src/pages/form/index.tsx
@@ -0,0 +1,14 @@
+import { ContactForm } from "@/components/ContactForm";
+
+const Form = () => {
+  return (
+    <main className="container relative mx-auto">
+      <div className="mx-auto lg:w-[800px] py-10 px-10">
+        <h1 className="text-2xl font-bold text-center pb-10">お問い合わせ</h1>
+        <ContactForm />
+      </div>
+    </main>
+  );
+};
+
+export default Form;
